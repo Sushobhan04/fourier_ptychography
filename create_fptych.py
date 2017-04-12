80a9703787589723068e6a4342d3fb775e2dc9de
Sushobhan04 <sushobhan04ghosh@gmail.com>
2017-04-11 22:49:07 -0500

    April 11

---
+++ create_fptych.py
@@ -0,0 +1,114 @@
+import numpy as np
+import numpy.fft as fft
+import matplotlib.pyplot as plt
+import matplotlib.image as im
+from PIL import Image
+import random
+import os
+import h5py
+
+
+def plot_arr(arr,name):
+	im.imsave(name+'.png',arr,cmap=plt.cm.gray)
+
+def add_dataset(data,label,grp):
+	grp.create_dataset("data",data=data)
+	grp.create_dataset("label",data = label)
+
+def shuffle_pairs(data, label):
+	c = list(zip(data, label))
+
+	random.shuffle(c)
+
+	data, label = zip(*c)
+
+	return np.array(data), np.array(label)
+
+def tvt_split(data,label, f, split = (0.8,0.1,0.1)):
+	l = data.shape[0]
+	train_pt = int(split[0]*l)
+	val_pt = int(split[1]*l)+train_pt
+
+	# data_sp = {}
+	train = f.create_group("train")
+	val = f.create_group("val")
+	test = f.create_group("test")
+
+	add_dataset(data[0:train_pt,],label[0:train_pt,],train)
+	add_dataset(data[train_pt:val_pt,],label[train_pt:val_pt,],val)
+	add_dataset(data[val_pt:,],label[val_pt:,],test)
+
+	# f['train'] = {'data':data[0:train_pt,], 'label':label[0:train_pt,]}
+	# f['val'] = {'data':data[train_pt:val_pt,], 'label':label[train_pt:val_pt,]}
+	# f['test'] = {'data':data[val_pt:,], 'label':label[val_pt:,]}
+
+	return f
+
+
+def patchify(img,size):
+	H = img.shape[0]
+	W = img.shape[1]
+
+	batch = []
+
+	for i in range((2*H)//size-1):
+		for j in range((2*W)//size-1):
+			x = i*size/2
+			y = j*size/2
+			batch.append(img[x:x+size,y:y+size])
+	return np.array(batch)
+
+def batch_fft(batch):
+	batch_f = []
+	for x in batch:
+		batch_f.append(fft_imgx)
+	return np.array(batch_f)
+
+def fft_img(img):
+	return fft.fftshift(fft.fft2(img))
+
+
+def filter_data(data,factor,thresh = (-1.0,1.0)):
+	d = data/factor
+	# d = np.where(d<thresh[0],0.0, d)
+	# d = np.where(d>thresh[1],0.0, d)
+
+	return d
+
+
+def create_dataset(source,destination):
+	dataset = []
+	labelset = []
+
+	for filename in os.listdir(source):
+		print filename
+		file = h5py.File(source+filename,'r')
+		dataset = np.array(file['data'])
+		labelset = np.array(file['label'])
+
+
+		f = h5py.File(destination+filename,'w')
+		dataset, labelset = shuffle_pairs(dataset, labelset)
+		f = tvt_split(dataset, labelset, f)
+		f.close()
+
+
+	print "dataset created"
+
+def main():
+	output_path = '/home/sushobhan/Documents/data/fourier_ptychography/datasets/pcp_ptych/'
+	# source = '/home/sushobhan/Documents/data/ptychography/data/Set91/'
+	source = '/home/sushobhan/Documents/data/fourier_ptychography/datasets/Test42_Set91_img512_patch48/train_images/'
+
+
+	create_dataset(source,output_path)
+
+
+
+	
+
+	# plot_arr(arr,'test_img')
+	# plot_arr(trans,'test_fft',t='c')
+
+if __name__=='__main__':
+	main()
--- residual_ptych.py
+++ residual_ptych.py
@@ -4,15 +4,18 @@ from keras.layers import (
     Activation,
     merge,
     Dense,
-    Flatten
+    Flatten,
+    Dropout,
+    Reshape
 )
 from keras.layers.convolutional import (
-    Convolution2D,
+    Conv2D,
     MaxPooling2D,
     AveragePooling2D,
-    Cropping2D
+    Conv2DTranspose
 )
 from keras.layers.normalization import BatchNormalization
+from keras.layers.merge import concatenate
 from keras import backend as K
 import numpy as np
 import h5py
@@ -21,9 +24,8 @@ from keras.utils import np_utils
 from keras import callbacks
 from keras.callbacks import LearningRateScheduler,EarlyStopping
 import math
-import math
 import sys
-import tensorflow as tf 
+from keras import losses
 
 K.set_image_dim_ordering('th')
 
@@ -37,218 +39,211 @@ else:
     COL_AXIS = 3
 
 
-# Helper to build a conv -> BN -> relu block
-def _conv_bn_relu(nb_filter, nb_row, nb_col, subsample=(1, 1),border_mode = 'same'):
-    def f(input):
-        conv = Convolution2D(nb_filter=nb_filter, nb_row=nb_row, nb_col=nb_col, subsample=subsample,
-                             init="he_normal", border_mode=border_mode)(input)
-        # conv = BatchNormalization(mode=0, axis=CHANNEL_AXIS)(conv)
-        return Activation("relu")(conv)
-
-    return f
-
-
-# Helper to build a BN -> relu -> conv block
-# This is an improved scheme proposed in http://arxiv.org/pdf/1603.05027v2.pdf
-def _bn_relu_conv(nb_filter, nb_row, nb_col, subsample=(1, 1)):
-    def f(input):
-        norm = BatchNormalization(mode=0, axis=CHANNEL_AXIS)(input)
-        activation = Activation("relu")(norm)
-        return Convolution2D(nb_filter=nb_filter, nb_row=nb_row, nb_col=nb_col, subsample=subsample,
-                             init="he_normal", border_mode="same")(activation)
-
-    return f
-
-
-# Adds a shortcut between input and residual block and merges them with "sum"
-def _shortcut(input, residual,border_mode = 'same'):
-    # Expand channels of shortcut to match residual.
-    # Stride appropriately to match residual (width, height)
-    # Should be int if network architecture is correctly configured.
-    # stride_width = input._keras_shape[ROW_AXIS] // residual._keras_shape[ROW_AXIS]
-    # stride_height = input._keras_shape[COL_AXIS] // residual._keras_shape[COL_AXIS]
-    # equal_channels = residual._keras_shape[CHANNEL_AXIS] == input._keras_shape[CHANNEL_AXIS]
-    # N = tf.shape(input)[2]
-    # M = tf.shape(residual)[2]
-
-    if border_mode=='valid':
-        shortcut = Cropping2D(cropping=((4,4),(4,4)))(input)
-    else:
-        shortcut = input
-    # shortcut = input
-    # # 1 X 1 conv if shape is different. Else identity.
-    # if stride_width > 1 or stride_height > 1 or not equal_channels:
-    #     shortcut = Convolution2D(nb_filter=residual._keras_shape[CHANNEL_AXIS],
-    #                              nb_row=1, nb_col=1,
-    #                              subsample=(stride_width, stride_height),
-    #                              init="he_normal", border_mode="same")(input)
-
-    return merge([shortcut, residual], mode="sum")
-
-
-# Builds a residual block with repeating bottleneck blocks.
-def _residual_block(block_function, nb_filters, repetitions, is_first_layer=False):
-    def f(input):
-        for i in range(repetitions):
-            init_subsample = (1, 1)
-            if i == 0 and not is_first_layer:
-                init_subsample = (2, 2)
-            input = block_function(nb_filters=nb_filters, init_subsample=init_subsample)(input)
-        return input
-
-    return f
-
-
-# Basic 3 X 3 convolution blocks.
-# Use for resnet with layers <= 34
-# Follows improved proposed scheme in http://arxiv.org/pdf/1603.05027v2.pdf
-def basic_block(nb_filters, init_subsample=(1, 1), kernel_shape = (5,5), border_mode = 'same'):
-    def f(input):
-        conv1 = _conv_bn_relu(nb_filters, kernel_shape[0],kernel_shape[1], subsample=init_subsample, border_mode = border_mode)(input)
-        residual = _conv_bn_relu(nb_filters, kernel_shape[0],kernel_shape[1], border_mode = border_mode)(conv1)
-        return _shortcut(input, residual, border_mode)
-
-    return f
-
-
-# Bottleneck architecture for > 34 layer resnet.
-# Follows improved proposed scheme in http://arxiv.org/pdf/1603.05027v2.pdf
-# Returns a final conv layer of nb_filters * 4
-def bottleneck(nb_filters, init_subsample=(1, 1)):
-    def f(input):
-        conv_1_1 = _bn_relu_conv(nb_filters, 1, 1, subsample=init_subsample)(input)
-        conv_3_3 = _bn_relu_conv(nb_filters, 3, 3)(conv_1_1)
-        residual = _bn_relu_conv(nb_filters * 4, 1, 1)(conv_3_3)
-        return _shortcut(input, residual)
-
-    return f
-
-def crop_tensor(set1,set2):
-    pass
-
 def crop(set,N):
     h = set.shape[2]
     w = set.shape[3]
 
     return set[:,:,N:h-N,N:w-N]
 
-def BatchGenerator(files,batch_size,border_mode):
+def BatchGenerator(files,batch_size,dtype = 'train'):
     while 1:
         for file in files:
             curr_data = h5py.File(file,'r')
-            data = np.array(curr_data['data'])
-            data = -data + 1.0
-            label = np.array(curr_data['label'])
-            label - -label + 1.0
-            if border_mode=='valid':
-                label = crop(label,10)
-            # print curr_data
-            # print file
-            # print data.shape
-            for i in range(data.shape[0]//batch_size + 1):
+            data = np.array(curr_data[dtype]['data'][()])
+            label = np.array(curr_data[dtype]['label'][()])
+            # print data.shape, label.shape
+
+            for i in range((data.shape[0]-1)//batch_size + 1):
                 # print 'batch: '+ str(i)
                 data_bat = data[i*batch_size:(i+1)*batch_size,]
                 label_bat = label[i*batch_size:(i+1)*batch_size,]
                 yield (data_bat, label_bat)
 
-def schedule(epoch):
-    if epoch<40:
-        return 0.0001
-    else:
-        return 0.0001
-
-def train_model(path_train,home,model_name,mParam):
-
-    # path_train = '/home/sushobhan/caffe/data/ptychography/databases/Test42_Set91_img512_patch48/train_images/'
-    # path_train =  "/home/sushobhan/Documents/data/ptychography/Test42_Set91_img512_patch48/train_images/"
-    # model_spec = sys.argv[1]
-
-    file = h5py.File(path_train+'set_1.h5','r')
-    ks = file.keys()
-    # print ks
-
-    data = file['data']
-    label = file['label']
-
+def TrainingSetGenerator(file):
+    curr_data = h5py.File(file,'r')
+    data = np.array(curr_data['data'])
+    label = np.array(curr_data['label'])
     print data.shape, label.shape
-    print np.max(data), np.max(label)
+    return data,label
+
+def schedule(epoch):
+    lr = 0.00001
+    if epoch<15:
+        return lr
+    elif epoch<150:
+        return lr/10
+    elif epoch<300:
+        return lr/40
+    elif epoch<450:
+        return lr/160
+    else:
+        return lr/1000
+
+def _residual_block(filters, kernel_size = 3, repetitions=1):
+    def f(input):
+        res = input
+        for i in range(repetitions):
+            res = _conv_relu(filters=filters,kernel_size=kernel_size)(res)
+
+        res = _conv(filters=filters,kernel_size=kernel_size)(res)
+
+        res = merge([res, input], mode = 'sum')
+        return res
+
+    return f
+
+def _conv_block(filters, kernel_size = 3, subsample=(1, 1)):
+    def f(input):
+        conv = Conv2D(filters=filters, kernel_size = (kernel_size,kernel_size), subsample=subsample,
+                             kernel_initializer="he_normal", padding="same")(input)
+        norm = BatchNormalization(mode=0, axis=CHANNEL_AXIS)(conv)
+        return Activation("relu")(norm)
+
+    return f
+
+def _dense_block(node_arr):
+    def f(input):
+        temp = input
+        for nodes in node_arr:
+            temp = Dense(nodes,kernel_initializer='he_normal')(temp)
+            temp = BatchNormalization(mode=0, axis=CHANNEL_AXIS)(temp)
+            temp = Activation("relu")(temp)
+        return temp
+
+    return f
+
+def _deconv_block(filters, kernel_size = 3, strides=(2, 2)):
+    def f(input):
+        conv = Conv2DTranspose(filters=filters, kernel_size = (kernel_size,kernel_size), strides=subsample,
+                             kernel_initializer="he_normal", border_mode="same")(input)
+        norm = BatchNormalization(mode=0, axis=CHANNEL_AXIS)(conv)
+        return Activation("relu")(norm)
+
+    return f
 
 
-    input_shape = data.shape[1:]
-    output_shape = label.shape[1:]
 
-    print input_shape,output_shape
+def _conv_relu(filters, kernel_size = 3, subsample=(1, 1)):
+    def f(input):
+        conv = Conv2D(filters=filters, kernel_size = (kernel_size,kernel_size), subsample=subsample,
+                             kernel_initializer="he_normal", border_mode="same")(input)
+        active = Activation("relu")(conv)
+        return active
 
-    # input_shape = (49,None,None)
-    # output_shape = (1,None,None)
-    print input_shape,output_shape
+    return f
 
-    border_mode = mParam['border_mode']
+def _conv_bn(filters, kernel_size = 3, subsample=(1, 1)):
+    def f(input):
+        conv = Conv2D(filters=filters, kernel_size = (kernel_size,kernel_size), subsample=subsample,
+                             kernel_initializer="he_normal", border_mode="same")(input)
+        norm = BatchNormalization(mode=0, axis=CHANNEL_AXIS)(conv)
+        return norm
+
+    return f
+
+def _conv(filters, kernel_size = 3, subsample=(1, 1)):
+    def f(input):
+        conv = Conv2D(filters=filters, kernel_size = (kernel_size,kernel_size), subsample=subsample,
+                             kernel_initializer="he_normal", border_mode="same")(input)
+        return conv
+
+    return f
+
+def create_model(input_shape, output_shape):
     input = Input(shape=input_shape)
 
-    temp = Convolution2D(128, 5, 5, border_mode=border_mode, init = 'he_normal')(input)
-    # temp = BatchNormalization(mode=0, axis=CHANNEL_AXIS)(temp)
-    temp = Activation('relu')(temp)
+    temp = _conv_relu(filters = 64, kernel_size = 5)(input)
 
-    temp = basic_block(128, kernel_shape = (3,3),border_mode = border_mode)(temp)
+    temp = _residual_block(filters = 64, kernel_size = 3, repetitions=1)(temp)
+    temp = _residual_block(filters = 64, kernel_size = 3, repetitions=1)(temp)
+    temp = _residual_block(filters = 64, kernel_size = 3, repetitions=1)(temp)
+    temp = _residual_block(filters = 64, kernel_size = 3, repetitions=1)(temp)
+    temp = _conv(filters = 1, kernel_size = 5)(temp)
 
-    # temp = basic_block(32, kernel_shape = (5,5))(temp)
+    # temp = concatenate([temp1,temp2],axis=CHANNEL_AXIS)
 
-    temp = Convolution2D(1, 3, 3, border_mode=border_mode, init = 'he_normal')(temp)
-    # temp = BatchNormalization(mode=0, axis=CHANNEL_AXIS)(temp)
-    temp = Activation('relu')(temp)
-    
 
-    model = Model(input=input, output=temp)
+    model = Model(input= input, output = temp)
+
+    return model
+
+
+def train_model(path_train,home,model_name,mParam):
 
     lrate = mParam['lrate']
     epochs = mParam['epochs']
     decay = mParam['decay']
     train_batch_size = mParam['train_batch_size']
     val_batch_size = mParam['val_batch_size']
-    samples_per_epoch = mParam['sam_epoch']
-    # sgd = Adadelta(lr=lrate, rho=0.95, epsilon=1e-08, decay=decay)
+    steps_per_epoch = mParam['steps_per_epoch']
+    validation_steps = mParam['validation_steps']
 
-    train_files = []
-    val_files = []
-    for i in range(1,5):
-        train_files.append(path_train+'set_'+str(i)+'.h5')
+    input_shape = mParam['input_shape']
+    output_shape = mParam['output_shape']
 
-    for i in range(5,6):
-        val_files.append(path_train+'set_'+str(i)+'.h5')
+    print input_shape,output_shape
 
-    # print files
-    train_generator = BatchGenerator(train_files,train_batch_size,border_mode)
-    val_generator = BatchGenerator(val_files,val_batch_size,border_mode)
-    lrate = LearningRateScheduler(schedule)
-    early_stop = EarlyStopping(monitor='val_loss', min_delta=0, patience=3, verbose=0, mode='auto')
-    callbacks_list = [lrate,early_stop]
+    border_mode = mParam['border_mode']
+    norm_axis = 1
 
-    sgd = SGD(lr=0.0, momentum=0.9, decay=decay, nesterov=True)
+    model = create_model(input_shape, output_shape)
+
+    # train_files = [path_train+'data/'+'dataset_1.h5']
+    # val_files = [path_train+'data/'+'valset_1.h5']
+    dataset = []
+
+    for i in range(1,6):
+        dataset.append(path_train+'datasets/pcp_ptych/'+'set_'+str(i)+'.h5')
+
+    train_generator = BatchGenerator(dataset,train_batch_size,dtype = 'train')
+    val_generator = BatchGenerator(dataset,val_batch_size,dtype = 'val')
+    lrate_sch = LearningRateScheduler(schedule)
+    early_stop = EarlyStopping(monitor='val_loss', min_delta=0, patience=50, verbose=0, mode='auto')
+    callbacks_list = [lrate_sch,early_stop]
+
+    sgd = SGD(lr=lrate, momentum=0.9, decay=decay, nesterov=True)
+
+    # model.compile(loss='mean_squared_error',
+    #           optimizer=sgd)
 
     model.compile(loss='mean_squared_error',
               optimizer=sgd)
 
-    model.fit_generator(train_generator,validation_data=val_generator,nb_val_samples=1000, samples_per_epoch = samples_per_epoch, nb_epoch = epochs,verbose=1 ,callbacks=callbacks_list)
+    # print validation_steps
 
-    model.save(home+'models/'+model_name+'.h5')
+    model.fit_generator(train_generator,validation_data=val_generator, validation_steps = validation_steps,steps_per_epoch = steps_per_epoch, epochs = epochs,verbose=1 ,callbacks=callbacks_list)
+    # model.fit(data, label, batch_size=train_batch_size, nb_epoch=epochs, verbose=1, callbacks=callbacks_list, validation_split=0.1, shuffle=True)
+    model.save(path_train+'models/'+model_name+'.h5')
 
     # print model.summary()
+
+
 def main():
-    path_train =  "/home/sushobhan/Documents/data/ptychography/Test42_Set91_img512_patch48/train_images/"
-    home = "/home/sushobhan/Documents/research/ptychography/"
+
+    path_train =  "/home/sushobhan/Documents/data/fourier_ptychography/"
+    home = "/home/sushobhan/Documents/research/fourier_ptychography/"
+    # set_name = sys.argv[1]
     model_name = sys.argv[1]
 
+    N = 64
+
     mParam = {}
-    mParam['lrate'] = 0.0001
-    mParam['epochs'] = 40
+    mParam['lrate'] = 0.001
+    mParam['epochs'] = 20
     mParam['decay'] = 0.0
-    mParam['train_batch_size'] = 512
-    mParam['sam_epoch'] = 10000
-    mParam['val_batch_size'] = 1000
     mParam['border_mode'] = 'same'
 
+    mParam['input_shape'] = (49,None, None)
+    mParam['output_shape'] = (1, None, None)
+
+    mParam['train_batch_size'] = 64
+    mParam['val_batch_size'] = 64
+    mParam['steps_per_epoch'] = 12000//mParam['train_batch_size']
+    mParam['validation_steps'] = 1500//mParam['val_batch_size']
+    # mParam['set_name'] = set_name
+
     train_model(path_train,home,model_name,mParam)
+
 
 if __name__ == '__main__':
     main()
